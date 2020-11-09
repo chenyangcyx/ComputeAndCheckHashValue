d035ee705dd6d1d45fd4a0c930b34b0312c5da23
chenyangcyx <chenyangcyx@outlook.com>
2020-11-10 00:27:03 +0800

    function improvement added hints for estimated remaining time for .Net Framwork and .Net Core
    commit time: 2020/11/10 00:26:59

--- ComputeMD5_CMD_NetFramwork_v2/Program.cs
+++ ComputeMD5_CMD_NetFramwork_v2/Program.cs
@@ -19,6 +19,24 @@ namespace ComputeMD5_CMD_NetFramwork_v2
             foreach (var path in all_path)
                 Console.WriteLine((++no) + ". " + path);
 
+            /*计算剩余时间 变量准备*/
+            int all_file_num = 0;
+            int handle_file_num = 0;
+            double all_handle_time_sum = 0d;
+            double handle_time_average = 0d;
+            foreach(var path in all_path)
+            {
+                if (Directory.Exists(path))
+                {
+                    foreach(var path_file in new DirectoryInfo(path).GetFiles())
+                    {
+                        if (!path_file.FullName.Contains(@"hash.md5"))
+                            ++all_file_num;
+                    }
+                }
+            }
+            /*计算剩余时间 变量准备*/
+
             DateTime before_all = DateTime.Now;
             Console.WriteLine("\n开始执行，开始时间：" + before_all.ToString("yyyy-MM-dd HH:mm:ss") + "\n");
             no = 0;
@@ -44,6 +62,10 @@ namespace ComputeMD5_CMD_NetFramwork_v2
                     DateTime file_after_time = DateTime.Now;
                     Console.WriteLine("      -result: " + result);
                     Console.WriteLine("      -开始时间：" + file_before_time.ToString("yyyy-MM-dd HH:mm:ss") + "，结束时间：" + file_after_time.ToString("yyyy-MM-dd HH:mm:ss") + "，总共同时：" + (file_after_time - file_before_time).TotalSeconds + " 秒");
+                    all_handle_time_sum += (file_after_time - file_before_time).TotalSeconds;
+                    ++handle_file_num;
+                    handle_time_average = all_handle_time_sum / handle_file_num;
+                    Console.WriteLine("      -预计剩余时间：" + handle_time_average * (all_file_num - handle_file_num)/60d + " 分，"+ handle_time_average * (all_file_num - handle_file_num) +" 秒");
                     sw.Write(result + " *" + file.Name + "\n");
                 }
                 sw.Flush();
